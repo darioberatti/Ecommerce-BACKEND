2c5130cd8600ed0dc2d760f8395f23be0f7a1f47
Dario Beratti <darioberatti@gmail.com>
2023-09-05 12:06:45 -0300

    server levantado, db creada y sincronizada

--- api/config/db/index.js
+++ api/config/db/index.js
@@ -1,1 +1,9 @@
 // configuracion de la DB
+const Sequelize = require("sequelize");
+const db = new Sequelize("ecommerce", null, null, {
+  host: "localhost",
+  dialect: "postgres",
+  logging: false,
+});
+
+module.exports = db;
--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
 // configuracion del SERVIDOR
+const express = require("express");
+const db = require("./config/db");
+
+const routes = require("./routes");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use("/api", routes);
+
+db.sync({ force: false })
+  .then(function () {
+    app.listen(PORT, () =>
+      console.log(`Servidor escuchando en el puerto ${PORT}`)
+    );
+  })
+  .catch(console.error);
