501cfdbe6a43019244096b15b8f1ebc22af0eb9b
martinferrando2005 <ferrandomartin6@gmail.com>
2023-09-05 15:03:04 -0300

    creacion de tabla "Cart"

--- api/config/db/index.js
+++ api/config/db/index.js
@@ -1,1 +1,10 @@
 // configuracion de la DB
+const Sequelize = require("sequelize")
+
+const db = new Sequelize('e-commerce', null, null, {
+  host: 'localhost',
+  dialect: 'postgres',
+  logging: false
+}); 
+
+module.exports = db; 
