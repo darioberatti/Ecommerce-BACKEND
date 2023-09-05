cd44a03fda521da9fbf1aadfcfc9fc3573d8e2ff
santimoran2720 <moran3490@gmail.com>
2023-09-05 15:29:28 -0300

    logout

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,23 +1,19 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const lista = []; // Lista negra para tokens invalidados
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const lista = [];
 
 const app = express();
 
-
-app.post('/users/logout',  (req, res) => {
-  const token = req.header('Authorization');
+app.post("/users/logout", (req, res) => {
+  const token = req.header("Authorization");
 
   if (!token) {
-    return res.status(401).json({ mensaje: 'Token no proporcionado' });
+    return res.status(401).json({ mensaje: "Token no proporcionado" });
   }
 
-  
   lista.push(token);
 
-  res.json({ mensaje: 'Logout exitoso' });
+  res.json({ mensaje: "Logout exitoso" });
 });
 
-module.exports=user;
-
-
+module.exports = user;
