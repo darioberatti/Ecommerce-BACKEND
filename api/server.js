e6865fc30c05f001a16ee956b44e125e5a9e68b1
martinferrando2005 <ferrandomartin6@gmail.com>
2023-09-05 18:50:01 -0300

    cambios hechos

--- api/server.js
+++ api/server.js
@@ -1,1 +1,29 @@
 // configuracion del SERVIDOR
+const express = require("express");
+const db = require("./config/db");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use("/api", routes);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+//Requerir los modelos desde index.js de models
+
+db.sync({ force: false })
+  .then(function () {
+    app.listen(PORT, () =>
+      console.log(`Servidor escuchando en el puerto ${PORT}`)
+    );
+  })
+  .catch(console.error);
