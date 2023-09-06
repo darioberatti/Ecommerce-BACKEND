71f5b920d76d7923d27e1f1bf0fd0ea6d6532619
santimoran2720 <moran3490@gmail.com>
2023-09-06 15:48:31 -0300

    ruta para obtener todos los productos

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Products } = require("../models");
 
 // Ruta para obtener todos los productos
-router.get("/products", (req, res, next) => {
+router.get("/", (req, res, next) => {
   Products.findAll()
     .then((productos) => {
       res.status(200).json(productos);
