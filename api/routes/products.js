dbd332d8dda19ede731c2f17bd332bfa3039f7ca
santimoran2720 <moran3490@gmail.com>
2023-09-06 14:12:40 -0300

    ruta para obtener todos los productos

---
+++ api/routes/products.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const { Products } = require("../models");
+
+// Ruta para obtener todos los productos
+router.get("/products", (req, res, next) => {
+  Products.findAll()
+    .then((productos) => {
+      res.status(200).json(productos);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+module.exports = router;
