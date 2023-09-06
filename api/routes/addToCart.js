0f785e2125ee8a174cd08483df830d5bd7fa30e0
santimoran2720 <moran3490@gmail.com>
2023-09-06 15:19:41 -0300

    ruta para agregar un producto al carrito

---
+++ api/routes/addToCart.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cartRouter = express.Router();
+const { Cart, Products } = require("../models"); 
+
+// Ruta para agregar un producto al carrito
+cartRouter.post("/addProductCart/:productId", (req, res, next) => {
+  const { user } = req; 
+
+  const productId = req.params.productId;
+  Products.findByPk(productId)
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({ message: "Producto no encontrado" });
+      }
+
+      
+      Cart.findOne({ where: { userId: user.id } })
+        .then((cart) => {
+          if (!cart) {
+            return Cart.create({ userId: user.id, completed: false });
+          }
+          return cart;
+        })
+        .then((cart) => {
+         
+          return cart.addProduct(product);
+        })
+        .then(() => {
+          res.status(201).json({ message: "Producto agregado al carrito correctamente" });
+        })
+        .catch((error) => {
+          next(error);
+        });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+module.exports = cartRouter;
