3613f7d4fe40cc95bc201c57d1093f577e243bde
Dario Beratti <darioberatti@gmail.com>
2023-09-07 11:09:26 -0300

    Se creó la ruta para eliminar un producto del carrito. Ver tema tabla intermedia. Falta probar las rutas

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -1,25 +1,23 @@
 const express = require("express");
 const cartRouter = express.Router();
-const { Cart, Products } = require("../models"); 
-
+const { Cart, Products } = require("../models");
 
 //Ruta para crear un carrito PARA REVISAR JUNTO AL FRONT
 
-cartRouter.post("/create", (req,res)=>{
+cartRouter.post("/create", (req, res) => {
   Cart.create(req.body)
     .then((newCart) => {
       res.status(201).json(newCart);
- })
+    })
     .catch((error) => {
       next(error);
     });
-})
-
+});
 
 // Ruta para agregar un producto al carrito PARA REVISAR JUNTO AL FRONT
 
 cartRouter.post("/:productId", (req, res, next) => {
-  const { user } = req; 
+  const { user } = req;
 
   const productId = req.params.productId;
   Products.findByPk(productId)
@@ -28,7 +26,6 @@ cartRouter.post("/:productId", (req, res, next) => {
         return res.status(404).json({ message: "Producto no encontrado" });
       }
 
-      
       Cart.findOne({ where: { userId: user.id } })
         .then((cart) => {
           if (!cart) {
@@ -37,11 +34,12 @@ cartRouter.post("/:productId", (req, res, next) => {
           return cart;
         })
         .then((cart) => {
-         
           return cart.addProduct(product);
         })
         .then(() => {
-          res.status(201).json({ message: "Producto agregado al carrito correctamente" });
+          res
+            .status(201)
+            .json({ message: "Producto agregado al carrito correctamente" });
         })
         .catch((error) => {
           next(error);
@@ -49,6 +47,27 @@ cartRouter.post("/:productId", (req, res, next) => {
     })
     .catch((error) => {
       next(error);
+    });
+});
+
+//Ruta para eliminar un producto del carrito PARA REVISAR JUNTO AL FRONT
+
+cartRouter.delete("/:productId", function (req, res, next) {
+  const { user } = req;
+
+  cart_products
+    .findOne({ where: { userId: user.id } }) //¿Hay que requerir la tabla intermedia en este archivo?
+    .then((cart) => {
+      cart
+        .destroy({
+          where: {
+            productId: req.params.productId,
+          },
+        })
+        .then(() => {
+          res.status(204).json({ message: "Producto eliminado del carrito" });
+        })
+        .catch(next);
     });
 });
 
