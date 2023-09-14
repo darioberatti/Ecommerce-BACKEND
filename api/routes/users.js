15c8c5aba29f4e7a541235877592b62bc725fb31
Dario Beratti <darioberatti@gmail.com>
2023-09-14 11:11:41 -0300

    Se modificaron las rutas get y put al userId para evitar hacer cambios en el email y la contraseña

--- api/routes/users.js
+++ api/routes/users.js
@@ -8,11 +8,19 @@ usersRouter.put("/:id", (req, res) => {
   const userId = req.params.id;
 
   Users.findByPk(userId).then((user) => {
+    if (req.body.email || req.body.password) {
+      return res
+        .status(400)
+        .send("No se pueden editar el email ni la contraseña");
+    }
     user
       .update(req.body, {
         returning: true,
       })
-      .then((response) => res.send(response));
+      .then((response) => res.send(response))
+      .catch((error) => {
+        res.status(500).send("Error al actualizar el usuario");
+      });
   });
 });
 
@@ -49,7 +57,7 @@ usersRouter.post("/login", async (req, res, next) => {
         email,
         name: user.name,
         lastname: user.lastname,
-        isAdmin: user.isAdmin
+        isAdmin: user.isAdmin,
       };
       const token = await generateToken(userData);
       res.cookie("token", token).send(userData);
@@ -76,21 +84,13 @@ usersRouter.get("/admin/all", validateUser, validateAdmin, (req, res) => {
     .catch((err) => console.log(err));
 });
 
-//Ruta para ver el historial de un usuario, FALTA TESTEAR
-// usersRouter.get("/:userId/history", (req, res) => {
-//   const userId = req.params.userId;
-//   let arrUserCarts=[]
-//   Users.findByPk(userId)
-//     .then((user) => user.history)
-//     .then((history) =>
-//       history?.map((cart) => {
-//         Cart.findByPk(cart.id)
-//           .then((userCart) => arrUserCarts.push(userCart))
-//           .catch((err) => console.log(err));
-//       })
-//     )
-//     .then(()=>res.send(arrUserCarts))
-//     .catch((err) => console.log(err));
-// });
+usersRouter.get("/:id", (req, res) => {
+  const userId = req.params.id;
+  Users.findByPk(userId, {
+    attributes: { exclude: ["password", "email"] },
+  }).then((user) => {
+    res.send(user);
+  });
+});
 
 module.exports = usersRouter;
