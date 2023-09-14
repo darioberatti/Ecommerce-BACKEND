251f20c18ee036a68fff6dc0c36fe57019e85c36
Mateo <mateo.navarro98@gmail.com>
2023-09-14 11:32:12 -0300

    Correcion seed, readme y arreglos de bugs

--- api/config/db/index.js
+++ api/config/db/index.js
@@ -1,6 +1,6 @@
 // configuracion de la DB
 const Sequelize = require("sequelize");
-const db = new Sequelize(process.env.DB_NAME, null, null, {
+const db = new Sequelize("ecommerce", null, null, {
   host: "localhost",
   dialect: "postgres",
   logging: false,
